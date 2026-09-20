632bcd8251fb11cf98a268a9985f500acd3775e1
AmrAhmed <amrahmed201135@gmail.com>
2026-09-21 00:12:46 +0300

    upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let btns = document.querySelectorAll(".btn");
+let sections = document.querySelectorAll(".section");
+let _remove = document.querySelector(".remove");
+
+console.log(sections);
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    remove();
+    sections.forEach((section) => {
+      if (btn.innerText == section.dataset.country) {
+        section.classList.remove("d-none");
+      }
+    });
+  });
+});
+_remove.addEventListener("click", () => {
+  sections.forEach((section) => {
+    remove();
+  });
+});
+
+function remove() {
+  sections.forEach((section) => {
+    section.classList.add("d-none");
+  });
+}
